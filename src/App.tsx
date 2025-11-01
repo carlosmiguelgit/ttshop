1e0a2e08d5e18c2d574a8d5c792139f607cd125b
[dyad] <carlosmiguelxiaomi@gmail.com>
2025-11-01 10:41:07 -0300

    [dyad] Mudando para HashRouter para corrigir o problema de página em branco na Hostinger. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -13,15 +13,18 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          {/* Rota principal que carrega o produto padrão */}
-          <Route path="/" element={<Index />} />
+          {/* Rota dinâmica para a página de detalhes do produto */}
+          <Route path="/produto/:slug" element={<Index />} />
+          
+          {/* Redireciona a rota raiz para o produto padrão (patinete) */}
+          <Route path="/" element={<Navigate replace to="/produto/patinete-eletrico-scooter-bluetooth" />} />
           
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
