5c7fec7507939ddfc1c427017b3bb3a6437ec25b
[dyad] <carlosmiguelxiaomi@gmail.com>
2025-11-10 04:08:54 -0300

    [dyad] Exibindo tela em branco temporariamente - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,5 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          {/* Rota dinâmica para a página de detalhes do produto */}
-          <Route path="/produto/:slug" element={<Index />} />
-          
-          {/* Redireciona a rota raiz para o produto padrão (iPhone 13) */}
-          <Route path="/" element={<Navigate replace to="/produto/iphone-13" />} />
-          
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return null;
+};
 
 export default App;
