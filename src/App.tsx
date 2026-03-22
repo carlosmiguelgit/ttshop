fc49e7ebc9b3e04efc140352018648e7c3b8ba97
[dyad] <carlosmiguelxiaomi@gmail.com>
2026-03-22 10:16:07 -0300

    [dyad] Aplicação temporariamente limpa com tela branca em todas as rotas para verificação. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,15 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Admin & Global
-import NotFound from "./pages/NotFound";
-import Admin from "./pages/Admin";
-
-// Componentes de Fluxo (Reutilizáveis)
-import Checkout from "./pages/Checkout";
-import AddAddress from "./pages/AddAddress";
-import AddCard from "./pages/AddCard";
-import PixPayment from "./pages/PixPayment";
-
-// Páginas de Produto
-import AspiradorProductPage from "./products/aspirador-de-po/ProductPage";
-import FuradeiraProductPage from "./products/furadeira/ProductPage";
-
-const queryClient = new QueryClient();
+// Componente de tela branca absoluta
+const BlankPage = () => <div className="min-h-screen bg-white" />;
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner position="top-center" />
-      <BrowserRouter>
-        <Routes>
-          {/* Admin */}
-          <Route path="/shopadmin" element={<Admin />} />
-          
-          {/* Rotas Dinâmicas por Produto (Garante que futuros produtos funcionem) */}
-          <Route path="/:productSlug/checkout" element={<Checkout />} />
-          <Route path="/:productSlug/endereco" element={<AddAddress />} />
-          <Route path="/:productSlug/cartao" element={<AddCard />} />
-          <Route path="/:productSlug/pix" element={<PixPayment />} />
-          
-          {/* Páginas Específicas de Produto */}
-          <Route path="/aspirador-de-po" element={<AspiradorProductPage />} />
-          <Route path="/furadeira" element={<FuradeiraProductPage />} />
-          
-          {/* Home */}
-          <Route path="/" element={<AspiradorProductPage />} />
-          
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<BlankPage />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
