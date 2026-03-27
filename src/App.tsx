b96ee38fa013d3ef9bcc3c4d396084b096ac1783
[dyad] <carlosmiguelxiaomi@gmail.com>
2026-03-26 21:54:08 -0300

    [dyad] Configurando redirecionamento automático para o Mercado Livre - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    window.location.href = "https://www.mercadolivre.com.br/rob-aspirador-eufy-omni-c20-3-em-1-estaco-tudo-em-um-succo-7000pa-corpo-ultrafino-85cm-esvaziamento-limpeza-e-secagem-automatica-mapeamento-laser-multinivel-escova-antiembaraco-wifi-app-voz-220v/p/MLB45873105?pdp_filters=item_id%3AMLB6072170418&from=gshop&matt_tool=49835134&matt_internal_campaign_id=&matt_word=&matt_source=google&matt_campaign_id=22090354217&matt_ad_group_id=194474656714&matt_match_type=&matt_network=g&matt_device=c&matt_creative=792355617033&matt_keyword=&matt_ad_position=&matt_ad_type=pla_with_promotion&matt_merchant_id=735128188&matt_product_id=MLB45873105-product&matt_product_partition_id=2454649971481&matt_target_id=pla-2454649971481&cq_src=google_ads&cq_cmp=22090354217&cq_net=g&cq_plt=gp&cq_med=pla_with_promotion&gad_campaignid=22090354217";
+  }, []);
+
   return (
     <div className="min-h-screen bg-white">
-      {/* Página em branco */}
+      {/* Redirecionando... */}
     </div>
   );
 };
